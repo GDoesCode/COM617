bbbf7e3853e29fb2478beb0030dddcfa5d35dc68
Gaetano Archer <5archg31@solent.ac.uk>
2024-03-25 18:05:08 +0000

    GitHub actions trial

--- Pages/UserBooking.razor.cs
+++ Pages/UserBooking.razor.cs
@@ -1,4 +1,5 @@
-﻿using COM617.Data;
+﻿using COM617.Components.Inputs;
+using COM617.Data;
 using COM617.Services;
 using Microsoft.AspNetCore.Components;
 
@@ -7,18 +8,21 @@ namespace COM617.Pages
     public partial class UserBooking
     {
         [Inject]
-        private VehicleService? VehicleService {  get; set; }
+        private VehicleService? VehicleService { get; set; }
 
-        private List<Vehicle> vehicles { get; set; } = new();
+        private List<Vehicle> Vehicles { get; set; } = new();
+
+        private SelectDropdown<VehicleType> Type { get; set; } = ; // Reference to the object.
 
         private void FilterVehicles()
         {
-            vehicles = VehicleService!.GetVehicles();
+            Vehicles = VehicleService!.GetVehicles();
         }
 
         private void InputChanged(string input)
         {
-            vehicles = VehicleService!.GetVehicles().Where(vehicle => vehicle.ModelName.Contains(input)).ToList();
+            VehicleType type = Type.Selection;
+            Vehicles = VehicleService!.GetVehicles().Where(vehicle => vehicle.ModelName.Contains(input) && vehicle.Type == type).ToList();
             StateHasChanged();
         }
 
